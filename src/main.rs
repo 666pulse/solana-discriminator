603d92bd97ababa15ece0e3baa7a6ccf79fdfab2
liushooter <shooter.hero@gmail.com>
2025-06-10 22:40:40 +0800

    up

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,15 @@ use hex;
 use std::fs::read_to_string;
 use std::path::Path;
 
-/// 计算 Solana 指令的 discriminator
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// 指令名称
-    #[arg(required = true)]
-    name: String,
+    #[arg(required_unless_present = "file")]
+    ins_name: Option<String>,
 
-    /// 命名空间
     #[arg(short = 'n', long, default_value = "global")]
     namespace: String,
 
-    /// 要读取的文件路径
     #[arg(short = 'f', long, value_parser = validate_file_type)]
     file: Option<String>,
 }
@@ -35,7 +31,7 @@ fn validate_file_type(path: &str) -> Result<String, String> {
 
     match path.extension().and_then(|ext| ext.to_str()) {
         Some("json") => Ok(path.to_string_lossy().into_owned()),
-        Some(ext) => Err(format!("不支持的文件类型：.{}", ext)),
+        Some(ext) => Err(format!("不支持的文件类型：.{}，只支持 json 格式", ext)),
         None => Err("文件没有扩展名".to_string()),
     }
 }
@@ -48,14 +44,14 @@ fn is_json_file(path: &str) -> bool {
         .unwrap_or(false)
 }
 
-fn print_instructions(json: &serde_json::Value) {
+fn print_discriminator(json: &serde_json::Value, namespace: &str) {
     if let Some(instructions) = json.get("instructions") {
         if let Some(instructions_array) = instructions.as_array() {
             println!("\n找到 instructions 节点，包含以下指令：");
             for (_, instruction) in instructions_array.iter().enumerate() {
                 if let Some(name) = instruction.get("name").and_then(|n| n.as_str()) {
                     // println!("{}. {}", index + 1, name);
-                    print_hash("global", name);
+                    print_hash(namespace, name);
                 }
             }
         } else {
@@ -77,14 +73,14 @@ fn parse_json_content(content: &str) -> Result<serde_json::Value, String> {
 }
 
 /// 处理 JSON 文件并打印指令信息
-fn process_json_file(path: &str) -> Result<(), String> {
+fn process_json_file(path: &str, namespace: &str) -> Result<(), String> {
     if !is_json_file(path) {
         return Err("不是 JSON 文件".to_string());
     }
 
     let content = read_file_content(path)?;
     let json = parse_json_content(&content)?;
-    print_instructions(&json);
+    print_discriminator(&json, namespace);
     Ok(())
 }
 
@@ -100,7 +96,6 @@ pub fn get_hash(namespace: &str, name: &str) -> [u8; 8] {
     sighash
 }
 
-/// 计算哈希值
 pub fn print_hash(namespace: &str, name: &str) {
     let hash = get_hash(namespace, name);
     let hash_hex = hex::encode(hash);
@@ -111,16 +106,19 @@ pub fn print_hash(namespace: &str, name: &str) {
     println!("hash: {:?}", hash);
     println!("hex: 0x{}", hash_hex);
     println!("base64: {}", base64_val);
-    println!("\n");
+    println!();
     ()
 }
 
 fn main() -> () {
     let args = Args::parse();
-    print_hash(&args.namespace, &args.name);
+
+    if let Some(ins_name) = args.ins_name {
+        print_hash(&args.namespace, &ins_name);
+    }
 
     if let Some(path) = args.file {
-        if let Err(e) = process_json_file(&path) {
+        if let Err(e) = process_json_file(&path, &args.namespace) {
             eprintln!("{}", e);
         }
     }
