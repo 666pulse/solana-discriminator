34117bd373693ad7c70094fae170df76aeca0589
liushooter <shooter.hero@gmail.com>
2025-06-12 10:46:07 +0800

    refactor: use solana-program directly instead of anchor-lang

--- src/main.rs
+++ src/main.rs
@@ -90,9 +90,7 @@ pub fn get_hash(namespace: &str, name: &str) -> [u8; 8] {
 
     let preimage = format!("{}:{}", namespace, snake_name);
     let mut sighash = [0u8; 8];
-    sighash.copy_from_slice(
-        &anchor_lang::solana_program::hash::hash(preimage.as_bytes()).to_bytes()[..8],
-    );
+    sighash.copy_from_slice(&solana_program::hash::hash(preimage.as_bytes()).to_bytes()[..8]);
     sighash
 }
 
